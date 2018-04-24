be7a6ee5a8677be84baa37e8aa7a5a93aa9541e8
theflyingmantis <rai.1@iitj.ac.in>
2018-04-24 20:18:29 +0530

    python 3 rules updated in code

--- app.py
+++ app.py
@@ -1,31 +1,33 @@
 from flask import Flask, request
 import json
 import requests
+import os
 
 app = Flask(__name__)
 
 # This needs to be filled with the Page Access Token that will be provided
 # by the Facebook App that will be created.
-PAT = ''
+PAT = os.environ["PAGE_ACCESS_TOKEN"]
 
 @app.route('/', methods=['GET'])
 def handle_verification():
-  print "Handling Verification."
+  print ("Handling Verification.")
   if request.args.get('hub.verify_token', '') == 'my_voice_is_my_password_verify_me':
-    print "Verification successful!"
+    print ("Verification successful!")
     return request.args.get('hub.challenge', '')
   else:
-    print "Verification failed!"
+    print ("Verification failed!")
     return 'Error, wrong validation token'
 
 @app.route('/', methods=['POST'])
 def handle_messages():
-  print "Handling Messages"
+  print ("Handling Messages")
   payload = request.get_data()
-  print payload
+  print (payload)
   for sender, message in messaging_events(payload):
-    print "Incoming from %s: %s" % (sender, message)
+    print ("Incoming from %s: %s" % (sender, message))
     send_message(PAT, sender, message)
+  print (sender)
   return "ok"
 
 def messaging_events(payload):
@@ -53,7 +55,7 @@ def send_message(token, recipient, text):
     }),
     headers={'Content-type': 'application/json'})
   if r.status_code != requests.codes.ok:
-    print r.text
+    print (r.text)
 
 if __name__ == '__main__':
   app.run()
