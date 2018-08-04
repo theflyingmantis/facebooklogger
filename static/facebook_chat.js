5e914323862810fd5c6e07d079b101a806d35a9b
Abhinav Rai <rai.1@iitj.ac.in>
2018-08-04 23:04:38 +0530

    facebook chat plugin added

---
+++ static/facebook_chat.js
@@ -0,0 +1,17 @@
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId            : '170251246934484',
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v3.1'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https//connect.facebook.net/en_US/sdk/xfbml.customerchat.js";;
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
