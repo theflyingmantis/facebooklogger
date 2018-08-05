a7aba8db04b7d8ce8bf1d0c65d67a5dca0cc313f
Abhinav Rai <rai.1@iitj.ac.in>
2018-08-05 22:38:20 +0530

    way-1 facebook chat plugin

--- static/js/facebook_chat.js
+++ static/js/facebook_chat.js
@@ -14,3 +14,5 @@ window.fbAsyncInit = function() {
   js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js#xfbml=1&version=v2.12&autoLogAppEvents=1';
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
+
+
