a6a51041d4ed8cf5236013a475c26b3b42b5dc24
Abhinav Rai <rai.1@iitj.ac.in>
2018-08-05 20:51:43 +0530

    Website UI changed. Added Fullpage JS

--- static/facebook_chat.js
+++ static/facebook_chat.js
@@ -1,17 +1,18 @@
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId            : '170251246934484',
-      autoLogAppEvents : true,
-      xfbml            : true,
-      version          : 'v3.1'
-    });
-  };
+alert('yo');
+window.fbAsyncInit = function() {
+  FB.init({
+    appId            : '170251246934484',
+    autoLogAppEvents : true,
+    xfbml            : true,
+    version          : 'v3.1'
+  });
+};
 
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk/xfbml.customerchat.js";;
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
+(function(d, s, id){
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement(s); js.id = id;
+   js.src = "//connect.facebook.net/en_US/sdk/xfbml.customerchat.js";;
+   fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'));
 
---
+++ static/js/facebook_chat.js
@@ -0,0 +1,16 @@
+window.fbAsyncInit = function() {
+  FB.init({
+    appId            : '170251246934484',
+    autoLogAppEvents : true,
+    xfbml            : true,
+    version          : 'v3.1'
+  });
+};
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js#xfbml=1&version=v2.12&autoLogAppEvents=1';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
