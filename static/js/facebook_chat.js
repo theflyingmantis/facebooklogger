81daa7c9b086e59e4de27d15d5091513711552ff
Abhinav Rai <rai.1@iitj.ac.in>
2018-08-05 23:19:13 +0530

    another try

--- static/js/facebook_chat.js
+++ static/js/facebook_chat.js
@@ -12,6 +12,6 @@ window.fbAsyncInit = function() {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js#xfbml=1&version=v2.12&autoLogAppEvents=1';
+  js.src = '//connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
